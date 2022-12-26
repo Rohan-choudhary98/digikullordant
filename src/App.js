893b12f665f4af32d6f2f850ef2bf64f213a8e63
Rohan-choudhary98 <103977021+Rohan-choudhary98@users.noreply.github.com>
2022-12-26 18:21:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Datafetch from './Datafetch';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Singlepage from './Singlepage';
 
 
